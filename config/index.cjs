259823d058a0573a9da5fb6e578b35611c34046d
yuhongwei00 <122715551+yuhongwei00@users.noreply.github.com>
2023-02-10 11:28:15 +0800

    lvmeinv

--- config/index.cjs
+++ config/index.cjs
@@ -8,44 +8,44 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxc96808f990f3b9d3',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'aee384dfb0031524ad795e18266ee7f3',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '辽宁',
+  CITY: '鞍山',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '吕美女',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'aee384dfb0031524ad795e18266ee7f3',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'i5bpenEMiddKZhnkfBMYaEqsCdAERfNWqEC7sfcljKE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '吕美女', year: '2002', date: '09-29',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+          type: '生日', name: '吕美女', year: '2002', date: '11-03',
+        }
+//         {
+//           type: '节日', name: '相识纪念日', year: '2020', date: '12-19',
+//         },
       ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
+//       // 我们在一起已经有xxxx天了的配置
+//       customizedDateList: [
+//         // 在一起的日子
+//         { keyword: 'love_day', date: '2022-09-08' },
+//         // 结婚纪念日
+//         { keyword: 'marry_day', date: '2022-09-09' },
+//       ],
     },
   ],
 
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: 'xiaoyu',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'aee384dfb0031524ad795e18266ee7f3',
     }
   ],
 
